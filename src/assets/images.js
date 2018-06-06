f6e761d00e52656e680a8cf1b1fd85ff215a261b
ARKALYK AKASH <arkalyk.akash@nu.edu.kz>
2018-06-06 12:20:00 +0200

    GoalsScreen plain UI created

---
+++ src/assets/images.js
@@ -0,0 +1,8 @@
+export const images = {
+  appIcon: require('../../resources/img/icon8Logo.png'),
+  chevronRight: require('../../resources/img/chevronRight.png'),
+  backgroundGrain: require('../../resources/img/backgroundGrain.png'),
+  beans: require('../../resources/img/imgBeans.png'),
+  dumbell: require('../../resources/img/imgDumbbell.png'),
+  mat: require('../../resources/img/imgMat.png'),
+};
---
+++ src/screens/GoalsScreen.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Image, Dimensions, ImageBackground, TouchableOpacity } from 'react-native';
+
+import { images } from '../assets';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
+const GoalItemView = (props) => (
+  <TouchableOpacity>
+    <View
+      style={{
+        backgroundColor: 'white',
+        height: 100,
+        marginTop: 20,
+        paddingHorizontal: 15,
+        borderRadius: 5,
+        marginHorizontal: 15,
+        alignItems: 'center',
+        flexDirection: 'row',
+        alignSelf: 'stretch',
+        justifyContent: 'space-between',
+        shadowOffset: { width: 2, height: 2 },
+        shadowColor: 'black',
+        shadowOpacity: 0.2,
+      }}
+    >
+      <View style={{ justifyContent: 'space-evenly' }}>
+        <Text style={{ fontSize: 20, fontWeight: 'bold', marginVertical: 4 }}>Lose weight</Text>
+        <Text style={{ fontSize: 16, marginVertical: 4 }}>Burn fat & get lean</Text>
+      </View>
+      <Image source={images.chevronRight} />
+    </View>
+  </TouchableOpacity>
+);
+
+class GoalsScreen extends Component {
+  render() {
+    return (
+      <ImageBackground source={images.backgroundGrain} style={{ flex: 1, justifyContent: 'center' }}>
+        <View style={{ position: 'absolute', flexDirection: 'row', justifyContent: 'space-between' }}>
+          <Image source={images.beans} />
+          <View style={{ alignItems: 'flex-end', flex: 1, justifyContent: 'flex-end', marginBottom: -40 }}>
+            <Image source={images.mat} />
+            <Image source={images.dumbell} style={{ marginTop: -270 }} />
+          </View>
+        </View>
+        <View style={styles.container}>
+          <Image source={images.appIcon} style={{ marginTop: 50 }} />
+          <Text style={{ marginTop: 8, fontSize: 12 }}>WELCOME TO 8FIT</Text>
+          <Text style={{ marginTop: 8, fontSize: 26, fontWeight: 'bold' }}>{"What's your goal"}</Text>
+          <View style={{ marginTop: 30, alignSelf: 'stretch' }}>
+            <GoalItemView />
+            <GoalItemView />
+            <GoalItemView />
+          </View>
+        </View>
+      </ImageBackground>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+});
+
+export { GoalsScreen };
